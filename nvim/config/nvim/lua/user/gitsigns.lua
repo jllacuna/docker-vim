58f0a0917c39683127e70627381fa1f1ff03efda
Jay Llacuna <626146+jllacuna@users.noreply.github.com>
2022-06-26 22:53:51 -0700

    Added keymaps for gitsigns

--- nvim/config/nvim/lua/user/gitsigns.lua
+++ nvim/config/nvim/lua/user/gitsigns.lua
@@ -5,6 +5,44 @@ if not status_ok then
 end
 
 gitsigns.setup {
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, { expr = true, desc = '[git] Next hunk' })
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, { expr = true, desc = '[git] Previous hunk' })
+
+    -- Actions
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = '[git] Stage hunk' })
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = '[git] Reset hunk' })
+    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = '[git] Undo stage hunk' })
+    map('n', '<leader>hS', gs.stage_buffer, { desc = '[git] Stage buffer' })
+    map('n', '<leader>hR', gs.reset_buffer, { desc = '[git] Reset buffer' })
+    map('n', '<leader>hp', gs.preview_hunk, { desc = '[git] Preview hunk' })
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = '[git] Blame line' })
+    map('n', '<leader>hB', gs.toggle_current_line_blame, { desc = '[git] Toggle blame current line' })
+    map('n', '<leader>hd', gs.diffthis, { desc = '[git] Diff unstaged' })
+    map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = '[git] Diff last commit'})
+    map('n', '<leader>ht', function() gs.toggle_word_diff(); gs.toggle_deleted() end, { desc = '[git] Toggle inline diff' })
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = '[git] inner hunk' })
+  end,
   signs = {
     add =          { hl = "GitSignsAdd",    text = "▎",  numhl = "GitSignsAddNr",    linehl = "GitSignsAddLn" },
     change =       { hl = "GitSignsChange", text = "▎",  numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
@@ -25,9 +63,10 @@ gitsigns.setup {
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
+    delay = 0,
     ignore_whitespace = false,
   },
+  current_line_blame_formatter = "  [<abbrev_sha>] <author_time> <author>: <summary>",
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
