599adcebd9db7ea4279300603c29db68f79f7280
Jay Llacuna <626146+jllacuna@users.noreply.github.com>
2022-06-28 13:01:15 -0700

    Removed auto_close code from nvim-tree. Can lead to nvim quitting without saving all buffers

--- nvim/config/nvim/lua/user/tree.lua
+++ nvim/config/nvim/lua/user/tree.lua
@@ -4,23 +4,6 @@ if not status_ok then
   return
 end
 
--- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
--- if not config_status_ok then
---   vim.notify "nvim-tree not found"
---   return
--- end
-
--- Replaces auto_close
--- local tree_cb = nvim_tree_config.nvim_tree_callback
--- vim.api.nvim_create_autocmd("BufEnter", {
---   nested = true,
---   callback = function()
---     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
---       vim.cmd "quit"
---     end
---   end
--- })
-
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 nvim_tree.setup {
